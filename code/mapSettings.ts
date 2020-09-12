e4c6194099ceea28f651d856bd0e34ba84478238
Jaedson Barbosa Serafim <jaedson33@gmail.com>
2020-09-12 18:53:00 -0300

    Criada primeira transição de página.

--- code/mapSettings.ts
+++ code/mapSettings.ts
@@ -1,21 +1,29 @@
 import { ContextoGeral } from "./contextoGeral";
 
-let contexto = new ContextoGeral(false, false)
+const contexto = new ContextoGeral(false, false)
 
-var ctrTempo1 = document.getElementById('ctrTempo1') as HTMLInputElement;
+const ctrTempo1 = document.getElementById('ctrTempo1') as HTMLInputElement;
 ctrTempo1.checked = contexto.escalaTempo === 1;
 
-var ctrTempo3 = document.getElementById('ctrTempo3') as HTMLInputElement;
+const ctrTempo3 = document.getElementById('ctrTempo3') as HTMLInputElement;
 ctrTempo3.checked = contexto.escalaTempo === 3;
 
-var ctrTempo12 = document.getElementById('ctrTempo12') as HTMLInputElement;
+const ctrTempo12 = document.getElementById('ctrTempo12') as HTMLInputElement;
 ctrTempo12.checked = contexto.escalaTempo === 12;
 
-var ctrTempo24 = document.getElementById('ctrTempo24') as HTMLInputElement;
+const ctrTempo24 = document.getElementById('ctrTempo24') as HTMLInputElement;
 ctrTempo24.checked = contexto.escalaTempo === 24;
 
-var ctrCorSuave = document.getElementById('ctrCorSuave') as HTMLInputElement;
+const ctrCorSuave = document.getElementById('ctrCorSuave') as HTMLInputElement;
 ctrCorSuave.checked = contexto.paletaCor === 0;
 
-var ctrCorAgressiva = document.getElementById('ctrCorAgressiva') as HTMLInputElement;
+const ctrCorAgressiva = document.getElementById('ctrCorAgressiva') as HTMLInputElement;
 ctrCorAgressiva.checked = contexto.paletaCor === 1;
+
+const form = document.getElementsByTagName('form')[0]
+const salvar = document.getElementById('ctrSalvar') as HTMLButtonElement
+salvar.onclick = ev => {
+    form.className = 'fade-out'
+    setTimeout(() => form.submit(), 2500)
+}
+setTimeout(() => form.className = 'fade-in', 1000)
